d96f2bee8635e795d6b4b6bbaa4c72143816d626
Ashwani-Pathak <146117884+Ashwani-Pathak@users.noreply.github.com>
2025-10-24 21:29:28 +0530

    Added Certificate struct, mapping, and owner variable to CertificateVerification contract

--- contracts/main.sol
+++ contracts/main.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.20;
 
 contract CertificateVerification {
-    // Contract logic will be added later
+    struct Certificate {
+        string studentName;
+        string courseName;
+        string issuedBy;
+        uint256 issueDate;
+        bool isValid;
+    }
+
+    mapping(bytes32 => Certificate) public certificates;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 }

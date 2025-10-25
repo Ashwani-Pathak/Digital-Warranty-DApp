18f10dc8aceab8b0c3b1575946ac12bd24e5ed98
Ashwani-Pathak <146117884+Ashwani-Pathak@users.noreply.github.com>
2025-10-25 22:20:25 +0530

    Added issueCertificate and verifyCertificate functions to CertificateVerification contract.

--- contracts/main.sol
+++ contracts/main.sol
@@ -11,10 +11,33 @@ contract CertificateVerification {
     }
 
     mapping(bytes32 => Certificate) public certificates;
-
     address public owner;
 
     constructor() {
         owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    function issueCertificate(
+        string memory studentName,
+        string memory courseName,
+        string memory issuedBy,
+        uint256 issueDate
+    ) public onlyOwner {
+        bytes32 certHash = keccak256(abi.encodePacked(studentName, courseName, issueDate));
+        certificates[certHash] = Certificate(studentName, courseName, issuedBy, issueDate, true);
+    }
+
+    function verifyCertificate(
+        string memory studentName,
+        string memory courseName,
+        uint256 issueDate
+    ) public view returns (bool) {
+        bytes32 certHash = keccak256(abi.encodePacked(studentName, courseName, issueDate));
+        return certificates[certHash].isValid;
     }
 }
